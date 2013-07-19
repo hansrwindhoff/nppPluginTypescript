341ed400c195bbbbb3655cad6187c5098be433cd
VWINDHA <hansrwindhoff@gmail.com>
2013-07-18 22:14:02 -0600

    initial code add

---
+++ NppPluginTypescript/Forms/frmMyDlg.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Drawing;
+using System.Text;
+using System.Windows.Forms;
+
+namespace NppPluginTypescript
+{
+    public partial class frmMyDlg : Form
+    {
+        public frmMyDlg()
+        {
+            InitializeComponent();
+        }
+
+        private void frmMyDlg_Load(object sender, EventArgs e)
+        {
+        }
+
+        private void linkLabel2_LinkClicked(object sender, LinkLabelLinkClickedEventArgs e)
+        {
+          this.linkLabel2.LinkVisited = true;
+          // Navigate to a URL.
+          System.Diagnostics.Process.Start("http://www.typescriptlang.org/");
+
+
+        }
+
+        private void linkLabel1_LinkClicked(object sender, LinkLabelLinkClickedEventArgs e)
+        {
+          this.linkLabel1.LinkVisited = true;
+          // Navigate to a URL.
+          System.Diagnostics.Process.Start("http://nodejs.org/");
+
+        }
+    }
+}
